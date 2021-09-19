54f1194d9db17bd6d5b6058b8bebbf9c1d3e8b3a
Paco Rosich <prosich@gmail.com>
2021-09-19 22:43:25 +0200

    wm era var. local

--- src/hola.cpp
+++ src/hola.cpp
@@ -3,12 +3,15 @@
 #include "config.h"
 #include "ver.h"
 
+const char app[]="demo";
+
 void setup() { 
-  Espini cosa(wifis(),"demo",ver,otasrv,logsrv);
+  Espini cosa(wifis(),app,ver,otasrv,logsrv);
   cosa.log("hola");
   delay(300e3);
   ESP.reset();
 }
 
 void loop() {
+  Serial1.println("HOLA");
 }
